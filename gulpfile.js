e6ace1d9bb1e9583c6892015af7cb0463029f149
webtoma <thomas.delalbre@gmail.com>
2023-09-27 11:03:46 +0200

    adding minify/rename to gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,25 @@
-const { series } = require('gulp');
+const { src, dest, series } = require('gulp');
+const babel = require('gulp-babel');
+const uglify = require('gulp-uglify');
+const rename = require('gulp-rename');
 
-function clean(cb){
+function cleanJS(){
   return src('src/js/*.js')
+        .pipe(babel())
+        .pipe(uglify())
+        .pipe(rename({ extname: '.min.js' }))
         .pipe(dest('public/js/'));
+}
+function cleanHTML(){
+  return src('src/*.html')
+        .pipe(dest('public/'));
+}
+function sass2css(){
+  
 }
 function build(cb) {
   cb();
 }
 
 exports.build = build;                    // export task so it can be used outside
-exports.default = series(clean, build);   // Run 2 tasks in series
+exports.default = series(cleanJS, cleanHTML, build);   // Run 2 tasks in series
--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,10 @@
+/* Comment on top of my file */
 console.log('test gulp');
+
+function test() {   
+    console.log('test function'); // TEST FUNCTION
+}
+
+
+/* Another comment */
+/* Last comment before the end of the world! */
