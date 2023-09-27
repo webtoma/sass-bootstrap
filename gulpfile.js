d4a7676a0dcefde84a9e0384585cf904c01eb950
webtoma <thomas.delalbre@gmail.com>
2023-09-27 12:08:26 +0200

    adding bootstrap and sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,9 @@
-const { src, dest, series } = require('gulp');
-const babel = require('gulp-babel');
-const uglify = require('gulp-uglify');
-const rename = require('gulp-rename');
+const { src, dest, series, watch }  = require('gulp');
+const babel                         = require('gulp-babel');
+const uglify                        = require('gulp-uglify');
+const rename                        = require('gulp-rename');
+const sass                          = require('gulp-sass')(require('sass'));
+const browserSync                   = require('browser-sync').create();
 
 function cleanJS(){
   return src('src/js/*.js')
@@ -14,12 +16,15 @@ function cleanHTML(){
   return src('src/*.html')
         .pipe(dest('public/'));
 }
-function sass2css(){
-  
+function cleanCSS(){
+  return src('src/scss/style.scss')
+        .pipe(sass({outputStyle: 'compressed'}))
+        .pipe(dest('public/css/'))
+        .pipe(browserSync.stream());
 }
 function build(cb) {
   cb();
 }
 
 exports.build = build;                    // export task so it can be used outside
-exports.default = series(cleanJS, cleanHTML, build);   // Run 2 tasks in series
+exports.default = series(cleanJS, cleanCSS, cleanHTML, build);   // Run 2 tasks in series
