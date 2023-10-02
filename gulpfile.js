a905a0fc2b6641831fb81cb4666ab0fa79553ec2
webtoma <thomas.delalbre@gmail.com>
2023-10-02 10:22:15 +0200

    updating, adding API exercice adding HTML templates

--- gulpfile.js
+++ gulpfile.js
@@ -4,13 +4,14 @@ const uglify                        = require('gulp-uglify');
 const rename                        = require('gulp-rename');
 const sass                          = require('gulp-sass')(require('sass'));
 const browserSync                   = require('browser-sync').create();
-const clean                           = require('gulp-clean');
+const clean                         = require('gulp-clean');
 
 function process_JS(){
   return src('src/js/*.js')
         .pipe(babel())
         .pipe(uglify())
         .pipe(rename({ extname: '.min.js' }))
+        .pipe(dest('backup/js/'))
         .pipe(dest('public/js/'))
         .pipe(browserSync.stream());
 }
@@ -23,11 +24,10 @@ function process_HTML(){
 function process_CSS(){
   return src('src/scss/style.scss')
         .pipe(sass({outputStyle: 'compressed'}))
+        .pipe(rename({ extname: '.min.css' }))
+        .pipe(dest('backup/css/'))
         .pipe(dest('public/css/'))
         .pipe(browserSync.stream());
-}
-function build(cb) {
-  cb();
 }
 function clean_All(){
   return src('public/*')
@@ -54,6 +54,7 @@ function watchFiles(){
   watch('src/scss/*.scss', series(clean_CSS, process_CSS)).on('change', browserSync.reload);
   watch('src/*.html', series(clean_HTML, process_HTML)).on('change', browserSync.reload);
 }
-exports.build = build;                    // export task so it can be used outside
-exports.default = series(clean_All, process_JS, process_CSS, process_HTML, build);   // Run 2 tasks in series
+
+// export task so it can be used outside
+exports.default = series(clean_All, process_JS, process_CSS, process_HTML);   // Run 2 tasks in series
 exports.watch = watchFiles;               // Run watchFiles task
